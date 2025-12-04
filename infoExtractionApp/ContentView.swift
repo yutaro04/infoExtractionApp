e58d8bb56b568c8a6da020baed120c9a3e10cfcf
yutaro04 <tezaki0410@icloud.com>
2025-12-04 21:17:25 +0900

    Initial Commit

---
+++ infoExtractionApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  infoExtractionApp
+//
+//  Created by 松尾悠太郎 on 2025/12/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ infoExtractionApp/infoExtractionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  infoExtractionAppApp.swift
+//  infoExtractionApp
+//
+//  Created by 松尾悠太郎 on 2025/12/04.
+//
+
+import SwiftUI
+
+@main
+struct infoExtractionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
